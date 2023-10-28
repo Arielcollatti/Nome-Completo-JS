71211ed828207b0267ed9dbbb6c92fdb957bbd62
arielcollatti <colatti2017@gmail.com>
2023-10-27 22:10:45 -0300

    exercicio realizado

---
+++ scrpit.js
@@ -0,0 +1,5 @@
+const nome = prompt('Qual o seu nome?');
+const sobrenome = prompt('Qual o seu sobrenome?');
+
+alert(`Seu nome completo é: ${nome} ${sobrenome}`);
+alert(`Seu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`);
